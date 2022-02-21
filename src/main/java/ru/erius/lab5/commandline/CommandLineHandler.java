151bd5fa7b8181406e0ff1375ffe7986eb158a3b
egor <egor_kapralov2003@mail.ru>
2022-02-22 00:35:55 +0300

    fixed terminal clear on linux

--- src/main/java/ru/erius/lab5/commandline/CommandLineHandler.java
+++ src/main/java/ru/erius/lab5/commandline/CommandLineHandler.java
@@ -58,8 +58,10 @@ public final class CommandLineHandler {
         try {
             if (System.getProperty("os.name").contains("Windows"))
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            else
-                Runtime.getRuntime().exec("clear");
+            else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
